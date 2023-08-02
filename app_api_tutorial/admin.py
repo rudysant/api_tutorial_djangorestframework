ba12522845f64bba9ce42bc2a3db42ecddae064e
rudysant <69287018+rudysant@users.noreply.github.com>
2023-08-02 21:21:40 +0700

    Update admin.py
    Edit admin

--- app_api_tutorial/admin.py
+++ app_api_tutorial/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from app_api_tutorial.models import Tutorial
 
-# Register your models here.
-admin.site.register(Tutorial)
+class Tutorials(admin.ModelAdmin):
+
+    list_display = ('id', 'title')
+    list_per_page = 10
+
+admin.site.register(Tutorial, Tutorials)
